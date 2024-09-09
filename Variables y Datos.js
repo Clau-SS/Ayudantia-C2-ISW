49236e4cc8a533c7db338b23d841756d157937a9
Clau-SS <claudia.sobino2101@alumnos.ubiobio.cl>
2024-09-08 23:26:04 -0300

    ADD primer ejercicio

---
+++ Variables y Datos.js
@@ -0,0 +1,18 @@
+//Declara una variable llamada nombre y asígnale un valor de tipo string
+const nombre = "Claudia";
+
+//Declara una variable llamada edad y asígnale un valor de tipo number
+const edad = 22;
+
+//Declara una variable llamada puedoConducir y asígnale un valor de tipo boolean
+const puedoConducir = true;
+
+//Para cada variable se debe imprimir por consola el resultado de cada variable declarada
+console.log("Mi Nombre:",nombre);
+console.log("Edad:",edad);
+console.log("Posee Liecencia:",puedoConducir);
+
+//Para cada variable se debe imprimir el tipo de dato que es por consola.
+console.log(typeof nombre);
+console.log(typeof edad);
+console.log(typeof puedoConducir);
