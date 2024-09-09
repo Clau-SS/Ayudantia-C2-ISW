127de5bb864cc28f423a674c9897d42fe96fa75d
Clau-SS <claudia.sobino2101@alumnos.ubiobio.cl>
2024-09-08 23:35:38 -0300

    ADD segundo ejercicio

---
+++ Operadores Aritmeticos.js
@@ -0,0 +1,12 @@
+const numero1 = 20;
+const numero2 = 40;
+//1. `Suma (+)`: Suma numero1 y numero2. Muestra el resultado por consola.
+console.log("La suma es:",numero1 + numero2)
+//2. `Resta (-)`: Resta numero1 y numero2. Muestra el resultado por consola.
+console.log("La resta es:",numero1 - numero2)
+//3. `Multiplicación (*)`: Multiplica numero1 y numero2. Muestra el resultado por consola.
+console.log("El producto es:",numero1 * numero2)
+//4. `División (/)`: Divide numero1 y numero2. Muestra el resultado por consola.
+console.log("La división es:",numero1 / numero2)
+//5. `Modulo (%)`:  Saca el modulo entre numero2 y numero1. Muestra el resultado por consola.
+console.log("El resto es:",numero2 % numero1)
