83f888d33f659aa12f8d38499d3edf3d20512663
Clau-SS <claudia.sobino2101@alumnos.ubiobio.cl>
2024-09-09 00:22:15 -0300

    ADD sexto ejercicio

---
+++ Funciones.js
@@ -0,0 +1,40 @@
+//1. Crea una función que reciba un string y retorne el string en mayúsculas.
+function Mayusculas(Frase){
+    return Frase.toUpperCase();
+}
+//ejemplo
+console.log(Mayusculas("vamos a pasar el ramo ? :("))
+
+//2. Crea una función que reciba un string y retorne el string en minúsculas.
+function Minusculas(Texto){
+    return Texto.toLowerCase();
+}
+
+//ejemplo 
+console.log(Minusculas("QUE DIFICIL SE VE EL SEMESTRE :( "))
+
+//3. Crear una función que reciba como parámetro 2 números y los reste.
+function Resta(a,b){
+    return a - b;
+}
+//ejemplo 
+console.log(Resta(5,3))
+
+//4. Crear una función que reciba como parámetro 2 números y los divida.
+function Division(a,b){
+    return a / b;
+}
+// ejemplo
+console.log(Division(9,3))
+//5. Crear una función que reciba como parámetro 2 números y los multiplique.
+function Multiplicacion(a,b){
+    return a * b;
+}
+//ejemplo
+console.log(Multiplicacion(10,10))
+//6. Crear una función que reciba un string y devuelva la longitud del string.
+function Longitud(Expresion){
+    return Expresion.length;
+}
+//ejemplo 
+console.log(Longitud("Aprendiendo nuevas cositas :)"))
